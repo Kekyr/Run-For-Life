fa37664ae906ee68dc6e1cc720bb0fa62d5bb65e
Mikhail Ostronosov <60397098+Kekyr@users.noreply.github.com>
2021-11-04 13:05:03 +0300

    Created player and camera movement.

---
+++ Assets/Main/Scripts/MovingCamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCamera : MonoBehaviour
+{
+    public Transform _target;
+    private Vector3 _offset = new Vector3(-0.57f, 3.354f, -3.32f);
+    private Vector3 _cameraVelocity;
+    private float _smoothTime = 0.001f;
+    
+
+
+    private void Start()
+    {
+        _target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    
+    private void FixedUpdate()
+    {
+
+        
+        var targetPosition = _target.transform.TransformPoint(_offset);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _cameraVelocity, _smoothTime);
+
+        
+        
+    }
+}
---
+++ Assets/Main/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float LineDistance = 4;
+    
+    [SerializeField] private int speed;
+    private CharacterController controller;
+
+    [SerializeField] private Transform camera;
+    private Vector3 targetCameraPosition;
+    private Vector3 _cameraVelocity;
+    [SerializeField] private float _smoothTime = 0.005f;
+
+    private Vector3 dir;
+
+    private static int currentLine = 1;
+    
+    private void Start()
+    {
+        dir = Vector3.forward;
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (SwipeController.swipeRight)
+        {
+            if (currentLine < 2)
+            {
+                currentLine++;
+            }
+        }
+
+        if (SwipeController.swipeLeft)
+        {
+            if (currentLine > 0)
+            {
+                currentLine--;
+            }
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if (currentLine == 0)
+        {
+            targetPosition += Vector3.left * LineDistance;
+            targetCameraPosition = camera.position+(Vector3.left * LineDistance);
+            camera.position = Vector3.SmoothDamp(camera.position, targetCameraPosition, ref _cameraVelocity, _smoothTime);
+            
+        }
+        else if (currentLine == 2)
+        {
+            targetPosition += Vector3.right * LineDistance;
+            targetCameraPosition = camera.position+(Vector3.right * LineDistance);
+            camera.position = Vector3.SmoothDamp(camera.position, targetCameraPosition, ref _cameraVelocity, _smoothTime);
+
+        }
+
+        transform.position = targetPosition;
+    }
+
+   
+    private void FixedUpdate()
+    {
+        dir.z = speed;
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+}
